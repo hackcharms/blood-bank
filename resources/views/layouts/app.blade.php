358b9938e8ffccaba92fc1205eebfdf836b593c1
hackcharms <hackcharms@gmail.com>
2020-10-27 02:24:20 +0530

    change in navbar style

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,7 +24,87 @@
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-danger text-white shadow-sm">
+        <header class="continer-fluid ">
+            <div id="menu-jk" class="header-bottom">
+                <div class="container">
+                    <div class="row nav-row">
+                        <div class="col-md-2 logo">
+                            <img src="{{asset('images/logo.jpg')}}" alt="">
+                        </div>
+                        <div class="col-md-10 nav-col">
+                            <nav class="navbar navbar-expand-lg navbar-light">
+
+                                <button
+                                    class="navbar-toggler"
+                                    type="button"
+                                    data-toggle="collapse"
+                                    data-target="#navbarNav"
+                                    aria-controls="navbarNav"
+                                    aria-expanded="false"
+                                    aria-label="Toggle navigation">
+                                    <span class="navbar-toggler-icon"></span>
+                                </button>
+                                <div class="collapse navbar-collapse" id="navbarNav">
+                                    <ul class="navbar-nav">
+                                        <li class="nav-item active">
+                                            <a class="nav-link" href="#">{{__('Home')}}
+                                            </a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="#about">{{__('About')}} Us</a>
+                                        </li>
+                                    
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="#gallery">{{__('Gallery')}}</a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="#contact">{{__('Contact US')}}</a>
+                                        </li>
+                                        
+                                        <li class="nav-item">
+                                            <a class="nav-link " href="{{route('consumer.hospitals')}}">{{__('Hosptals')}}</a>
+                                        </li>
+                                        @guest
+                                            <li class="nav-item">
+                                                <a class="nav-link " href="{{ route('login') }}">{{ __('Login') }}</a>
+                                            </li>
+                                            @if (Route::has('register'))
+                                                <li class="nav-item">
+                                                    <a class="nav-link " href="{{ route('register') }}">{{ __('Register') }}</a>
+                                                </li>
+                                            @endif
+                                        @else
+                                            <li class="nav-item dropdown">
+                                                <a id="navbarDropdown" class="nav-link dropdown-toggle " href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                                    {{ Auth::user()->name }}
+                                                </a>
+
+                                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                                    <a class="dropdown-item " href="{{ route('consumer.profile.show') }}">
+                                                        {{ __('Profile') }}
+                                                    </a>
+                                                    <a class="dropdown-item " href="{{ route('logout') }}"
+                                                        onclick="event.preventDefault();
+                                                                        document.getElementById('logout-form').submit();">
+                                                        {{ __('Logout') }}
+                                                    </a>
+
+                                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                                        @csrf
+                                                    </form>
+                                                    
+                                                </div>
+                                            </li>
+                                        @endguest
+                                    </ul>
+                                </div>
+                            </nav>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
+        {{-- <nav class="navbar navbar-expand-md navbar-light bg-danger text-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand text-white" href="{{ url('/') }}">
                     {{ config('app.name', 'Laravel') }}
@@ -37,8 +117,9 @@
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
                         <li class="nav-item">
-                                <a class="nav-link text-white" href="{{ route('consumer.hospitals') }}">{{ __('Blood Request') }}</a>
+                                <a class="nav-link text-white" href="{{ route('consumer.hospitals') }}">{{ __('Hospitals') }}</a>
                             </li>
+
                     </ul>
 
                     <!-- Right Side Of Navbar -->
@@ -79,7 +160,7 @@
                     </ul>
                 </div>
             </div>
-        </nav>
+        </nav> --}}
 
         <main class="">
             <div class="raw">
